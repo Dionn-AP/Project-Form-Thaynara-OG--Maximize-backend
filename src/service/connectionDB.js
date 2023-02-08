3dac34c928dc1d37afc3bef3bfeba1c8823f5327
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-08 13:17:59 -0300

    doc: configuration for connection to the api database - Supabase

---
+++ src/service/connectionDB.js
@@ -0,0 +1,11 @@
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+    }
+});
+
+module.exports = knex;
