7d30c5663dcedd9bba5655f776ecb2295ab8f3ea
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-08 13:14:17 -0300

    doc: deleting connection settings with Mongo DB Atlas

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routes);
 
-mongoose.set('strictQuery', false)
-
-mongoose.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vkbkvxp.mongodb.net/bancoapicontact?retryWrites=true&w=majority`
-)
-    .then(() => {
-        console.log('Conectado ao MongoDB!')
-        app.listen(8000)
-    })
-    .catch((err) => console.log(err));
+app.listen(3333);
