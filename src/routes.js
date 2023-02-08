1854b1d38fd0885fe65049ce29e200c1f49a4d4a
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-08 13:13:33 -0300

    doc: configuring routes and endpoints

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,26 @@
 const express = require('express');
 
-import Usercontroller from './controllers/user';
-import SentEmails from './controllers/sentEmails';
-import Login from './controllers/login';
-import authMiddleware from './middleware/middlewareLogin';
+const { createuser, getuser, replaymessage } = require('./controllers/user');
+const { sendemail, viewmessages, messageread } = require('./controllers/sentEmails');
+const { authenticate } = require('./controllers/login');
+const MiddlewareLogin = require('./middleware/middlewareLogin');
 
 const router = express();
 
 //user
-router.post('/user', Usercontroller.createuser);
-router.post('/login', Login.authenticate);
+router.post('/user', createuser);
+router.post('/login', authenticate);
 
 //sent emails
-router.post('/sendmail', SentEmails.sendemail);
+router.post('/sendmail', sendemail);
+
+router.use(MiddlewareLogin);
 
 //authenticated routes
-router.get('/user', authMiddleware, Usercontroller.getuser);
+router.get('/user', getuser);
+router.post('/replay', replaymessage);
+
+router.get('/viewmessages', viewmessages);
+router.patch('/messageread/:id', messageread);
+
+module.exports = router;
